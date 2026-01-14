caf01293e76e9e9298b407eac5c1ac634c9dac04
Ron Munitz <ron@thepscg.com>
2026-01-14 20:27:14 +0100

    pscg_debos: added automatic DHCP on network interfaces if config_pscgdebos__network_manager is systemd-networkd

--- layers/pscg_debos/network_minimal/add-layer.sh
+++ layers/pscg_debos/network_minimal/add-layer.sh
@@ -7,9 +7,26 @@
 
 LOCAL_APT_FLAGS="--no-install-recommends"
 
+do_auto_dhcp_in_systemd_networkd() {
+	if [ "${config_pscgdebos__network_manager}" = "systemd-networkd" ] ; then
+		info "Auto enabling systemd-networkd with DHCP"
+		sudo tee << EOF $ROOTFS_DIR/etc/systemd/network/20-wired.network > /dev/null || fatalError "Failed to create network enabling file"
+[Match]
+Name=en* wl* eth* wlan*
+
+[Network]
+DHCP=yes
+EOF
+	# Note that systemd does not enable a network service by default. So we enable it in the next line
+	sudo chroot $ROOTFS_DIR bash -c "systemctl enable systemd-networkd" || fatalError "Failed to enable systemd-networkd.service"
+	fi
+}
+
 main() {
 	source_file_or_die $LOCAL_DIR/packages.deb.buildconfig # the package config is sourced but there will not be duplication of packages (from the other sourcing for cache building) so that is fine
 	sudo chroot $ROOTFS_DIR bash -c "DEBIAN_FRONTEND=noninteractive ;  apt-get install -y $LOCAL_APT_FLAGS $pscg_debos_ubuntu_network_packages " || fatalError "Failed to run main logic"
+
+	do_auto_dhcp_in_systemd_networkd
 }
 
 
