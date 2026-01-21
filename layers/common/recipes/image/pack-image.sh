a47dc8c035e2b44c4ad38e8cac4740d8f855d266
Ron Munitz <ron@thepscg.com>
2026-01-21 23:16:05 +0100

    imager: added the (optional) execution of config_bsp__imager__pre_common_list_of_image_creation_scripts_to_run config_bsp__imager__post_common_list_of_image_creation_scripts_to_run to allow a BSP add additional steps prior to the common packaging and after it

--- layers/common/recipes/image/pack-image.sh
+++ layers/common/recipes/image/pack-image.sh
@@ -6,7 +6,10 @@ main() {
 	. $LOCAL_DIR/imager.buildconfig
 
 	for script in $config_imager__staging_list_of_image_creation_scripts_to_run ; do
-		info_do_or_die $LOCAL_DIR/staging/$script
+		if [[ "$script" != /* ]]; then
+			script=$LOCAL_DIR/staging/$script
+		fi
+		info_do_or_die $script
 	done
 
 	if [ "$config_imager__staging_do_non_staging_stuff" = "false" ] ; then
@@ -14,8 +17,15 @@ main() {
 		return
 	fi
 
-	for script in $config_imager__list_of_image_creation_scripts_to_run ; do
-			info_do_or_die $LOCAL_DIR/$script
+	for script in $config_bsp__imager__pre_common_list_of_image_creation_scripts_to_run \
+		$config_imager__list_of_image_creation_scripts_to_run \
+		$config_bsp__imager__post_common_list_of_image_creation_scripts_to_run \
+		; do
+		if [[ "$script" != /* ]]; then
+			script=$LOCAL_DIR/$script
+		fi
+		debug $script
+		info_do_or_die $script
 	done
 }
 
